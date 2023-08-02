c48774d78f972a4ebe0011d9cb28f1545af5a4bd
oryzaaa <oryzasativacikal@gmail.com>
2023-08-02 20:06:53 +0700

    Resolve Embed

--- src/pages/EmailAplication.jsx
+++ src/pages/EmailAplication.jsx
@@ -1,4 +1,3 @@
-import ButtonSidebar from "../components/Element/ButtonSidebar/ButtonSidebar"
 import AuthLayout from "../components/Layout/AuthLayout"
 
 function EmailAplication() {
@@ -6,9 +5,11 @@ function EmailAplication() {
         <AuthLayout>
             <h1>Halaman Email</h1>
 
-            <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowFullScreen></iframe>
+            {/* untuk mengambil vidio dari youtube nya dalah dengan mengambil link terakhir dari vidio youtube nya */}
+            <div className="ratio modulLearning ratio-16x9">
+                <iframe className="modulLearning-item" src="https://www.youtube.com/embed/lkIFF4maKMU" title="YouTube video" allowFullScreen></iframe>
             </div>
+
         </AuthLayout>
     )
 }
